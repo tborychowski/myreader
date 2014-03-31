eb7be2c88858ec9b8d9a5fc1b4e1e1e0c95d6839
Tomek <tborychowski@gmail.com>
2014-03-31 23:06:48 +0100

    refactoring

---
+++ application/views/home/admin.blade.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en" class="admin">
+<meta charset="utf-8">
+<title>MyReader</title>
+{{Asset::styles()}}
+<body>
+ADMIN
+</body>
---
+++ application/views/home/admin_login.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en" class="admin">
+<meta charset="utf-8">
+<title>MyReader</title>
+{{Asset::styles()}}
+<body>
+{{Form::open()}}
+	{{Form::label('username', 'Username')}}
+	{{Form::text('username', null, array('autofocus'=>'true', 'autocomplete'=>'off'))}}
+	{{Form::label('password', 'Password')}}
+	{{Form::password('password')}}
+	{{Form::button('submit')}}
+{{Form::close()}}
+</body>
