25b88141d37e4204d583feaab3882c6f58eed59b
Tom <tborychowski@gmail.com>
2015-07-28 21:50:07 +0100

    center toolbar title

--- resources/views/sources/toolbar-add.blade.php
+++ resources/views/sources/toolbar-add.blade.php
@@ -1,10 +1,3 @@
-<a class="btn" href="{{ url('source ') }}">
-    {{-- <i class="fa fa-arrow-left"></i> --}}
-    <i class="icon ion-ios-arrow-left"></i>
-</a>
-
-<h1 class="title">{{ $delFormAction ? 'Edit source' : 'Add new source' }}</h1>
-
 @if ($delFormAction)
     {!! Form::open($delFormAction) !!}
         <button type="submit" class="btn btn-right" title="Delete source">
@@ -14,3 +7,9 @@
     {!! Form::close() !!}
 @endif
 
+<a class="btn" href="{{ url('source ') }}">
+    {{-- <i class="fa fa-arrow-left"></i> --}}
+    <i class="icon ion-ios-arrow-left"></i>
+</a>
+
+<h1 class="title">{{ $delFormAction ? 'Edit source' : 'Add new source' }}</h1>
--- resources/views/sources/toolbar.blade.php
+++ resources/views/sources/toolbar.blade.php
@@ -1,11 +1,3 @@
-<a class="btn" href="{{ url('/') }}">
-    {{-- <i class="fa fa-arrow-left"></i> --}}
-    <i class="icon ion-ios-arrow-left"></i>
-</a>
-
-<h1 class="title">Manage Sources</h1>
-
-
 <a class="btn btn-right" href="{{ url('import') }}" title="Import sources from OPML file">
     {{-- <i class="fa fa-download"></i> --}}
     <i class="icon ion-ios-cloud-upload-outline"></i>
@@ -15,4 +7,13 @@
     {{-- <i class="fa fa-plus"></i> --}}
     <i class="icon ion-ios-plus-outline"></i>
 </a>
+
+
+<a class="btn" href="{{ url('/') }}">
+    {{-- <i class="fa fa-arrow-left"></i> --}}
+    <i class="icon ion-ios-arrow-left"></i>
+</a>
+
+<h1 class="title">Manage Sources</h1>
+
 
